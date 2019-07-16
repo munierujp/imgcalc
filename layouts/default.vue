f496b61060fc9f12c241c793810cc2cf9c8717ab
Munieru <20086673+munierujp@users.noreply.github.com>
2019-07-16 11:24:07 +0900

    Update layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,20 @@
 <template>
-  <v-app>
+  <v-app id="app">
     <v-content>
-      <nuxt />
+      <v-container
+        grid-list-xl
+        text-xs-center
+      >
+        <nuxt />
+      </v-container>
     </v-content>
   </v-app>
 </template>
+
+<style lang="scss">
+#app {
+  background-color: #fafafa;
+  color: #212121;
+  font-family: Roboto, "Noto Sans JP", sans-serif;
+}
+</style>
