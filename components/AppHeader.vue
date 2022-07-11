259c8953fc4d54f796975c86c73f55b40dece89e
Munieru <20086673+munierujp@users.noreply.github.com>
2022-07-11 22:10:11 +0900

    Fix AppHeader.vue

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,17 +1,16 @@
 <template>
-  <v-toolbar
+  <v-app-bar
     class="app-header grey--text text--lighten-5"
     color="grey darken-4"
     dark
+    app
   >
-    <v-toolbar-title>
-      {{ $t('APP_NAME') }}
-    </v-toolbar-title>
+    <v-toolbar-title>{{ $t('APP_NAME') }}</v-toolbar-title>
     <v-spacer />
     <v-flex xs4 sm2 md2 lg1>
       <app-language-switcher />
     </v-flex>
-  </v-toolbar>
+  </v-app-bar>
 </template>
 
 <script>
