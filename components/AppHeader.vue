d3bc70b13f35e9bb7f51930b38bf5e0937a6a02d
Munieru <20086673+munierujp@users.noreply.github.com>
2023-02-18 19:29:13 +0900

    Update AppHeader.vue

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -3,8 +3,7 @@
     class="app-header grey--text text--lighten-5"
     color="grey darken-4"
     dark
-    app
   >
-    <v-toolbar-title>imgcalc</v-toolbar-title>
+    <v-app-bar-title>imgcalc</v-app-bar-title>
   </v-app-bar>
 </template>
