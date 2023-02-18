e71efff9599f7e2633694236377f94ccdf922d79
Munieru <20086673+munierujp@users.noreply.github.com>
2023-02-18 15:52:10 +0900

    Update Nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  build: {
+    transpile: ['vuetify']
+  },
+  css: [
+    'vuetify/lib/styles/main.sass'
+  ],
+  vite: {
+    define: {
+      'process.env.DEBUG': false
+    }
+  }
 })
