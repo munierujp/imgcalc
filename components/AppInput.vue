4ce182f896d9e04cef3f465dc980ef83fc3a9a27
Munieru <20086673+munierujp@users.noreply.github.com>
2023-02-18 16:24:04 +0900

    Create AppInput.vue

---
+++ components/AppInput.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-text-field
+    v-model.number="computedValue"
+    :label="label"
+    :min="min"
+    type="number"
+  />
+</template>
+
+<script>
+// TODO: TS化
+
+export default {
+  props: {
+    value: {
+      type: Number,
+      required: true
+    },
+    label: {
+      type: String,
+      required: true
+    },
+    min: {
+      type: Number,
+      default: 1
+    }
+  },
+  computed: {
+    computedValue: {
+      get () {
+        return this.value
+      },
+      set (value) {
+        const num = isNumber(value) ? value : this.min
+        this.$emit('input', num)
+      }
+    }
+  }
+}
+
+function isNumber (value) {
+  return typeof value === 'number'
+}
+</script>
