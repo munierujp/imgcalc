389741e19303347a576a581a68c87e7878d199e1
Munieru <20086673+munierujp@users.noreply.github.com>
2023-02-18 16:50:00 +0900

    Create default.vue

---
+++ layouts/default.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-app id="app">
+    <AppHeader />
+    <v-main>
+      <v-container
+        grid-list-xl
+        text-xs-center
+      >
+        <slot />
+      </v-container>
+    </v-main>
+    <AppFooter />
+  </v-app>
+</template>
+
+<script>
+// TODO: TS化
+
+import AppHeader from '../components/AppHeader'
+import AppFooter from '../components/AppFooter'
+
+export default {
+  head () {
+    return {
+      title: 'imgcalc',
+      meta: [
+        { hid: 'description', name: 'description', content: 'Web app for calculating image size from aspect ratio' },
+        { hid: 'og:title', property: 'og:title', content: 'imgcalc' },
+        { hid: 'og:description', property: 'og:description', content: 'Web app for calculating image size from aspect ratio' }
+      ]
+    }
+  },
+  components: {
+    AppHeader,
+    AppFooter
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  background-color: #fafafa;
+  color: #212121;
+  font-family: Roboto, "Noto Sans JP", sans-serif;
+}
+</style>
