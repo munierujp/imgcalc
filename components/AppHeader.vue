f1f4bcee0dc249fad7e94ea09f6a1c4af2a0a83c
Munieru <20086673+munierujp@users.noreply.github.com>
2023-02-18 21:53:34 +0900

    Add GitHub link to header

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,5 +1,12 @@
 <template>
   <v-app-bar class="app-header bg-grey-darken-4 text-grey-lighten-5">
-    <v-app-bar-title>imgcalc</v-app-bar-title>
+    <v-app-bar-title text="imgcalc" />
+    <v-btn
+      icon
+      href="https://github.com/munierujp/imgcalc"
+      target="_blank"
+    >
+      <v-icon icon="mdi-github" />
+    </v-btn>
   </v-app-bar>
 </template>
