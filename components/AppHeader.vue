8c8b95f3866b1ecd788d38db35601ae69a9ff61e
Munieru <20086673+munierujp@users.noreply.github.com>
2023-02-18 16:27:46 +0900

    Create AppHeader.vue

---
+++ components/AppHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app-bar
+    class="app-header grey--text text--lighten-5"
+    color="grey darken-4"
+    dark
+    app
+  >
+    <v-toolbar-title>{{ $t('APP_NAME') }}</v-toolbar-title>
+  </v-app-bar>
+</template>
+
+<script>
+// TODO: TS化
+
+import AppLanguageSwitcher from '~/components/AppLanguageSwitcher'
+
+export default {
+  components: {
+    AppLanguageSwitcher
+  }
+}
+</script>
