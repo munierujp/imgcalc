d0547fc3198592574aa2465080ab6157eb392b69
Munieru <20086673+munierujp@users.noreply.github.com>
2023-02-18 16:32:07 +0900

    Create AppFooter.vue

---
+++ components/AppFooter.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-footer
+    color="grey darken-4"
+    class="app-footer grey--text text--lighten-5"
+  >
+    <v-spacer />
+    <div>
+      Created by 
+      <a
+        href="https://twitter.com/munieru_jp"
+        target="_blank"
+        place="creator"
+      >@munieru_jp</a>
+      <span>-</span>
+      Hosted on 
+      <a
+        href="https://github.com/munierujp/imgcalc"
+        target="_blank"
+        place="site"
+      >GitHub</a>
+    </div>
+    <v-spacer />
+  </v-footer>
+</template>
+
+<style lang="scss" scoped>
+.app-footer {
+  a {
+    color: inherit;
+    text-decoration: inherit;
+  }
+}
+</style>
