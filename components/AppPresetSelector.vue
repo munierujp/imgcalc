1614c5bf401fcbacb8e8e0a26373442cbdcadcda
Munieru <20086673+munierujp@users.noreply.github.com>
2023-02-19 15:40:35 +0900

    Create AppPresetSelector.vue

---
+++ components/AppPresetSelector.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+import type { Preset } from '../types/Preset';
+
+const props = defineProps({
+  items: {
+    type: Array as PropType<Preset[]>,
+    required: true
+  },
+  label: {
+    type: String,
+    required: true
+  },
+  modelValue: {
+    type: Object as PropType<Preset>,
+    required: true
+  }
+})
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: Preset): void
+}>();
+
+const value = computed({
+  get () {
+    return props.modelValue
+  },
+  set (value) {
+    emit('update:modelValue', value)
+  }
+})
+</script>
+
+<template>
+  <v-select
+    :label="label"
+    :items="items"
+    item-title="label"
+    item-value="value"
+    return-object
+    v-model="value"
+  />
+</template>
