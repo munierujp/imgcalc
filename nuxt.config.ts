80bc9f52947943efdd227dd0bae8a5aa5f9995a0
Munieru <20086673+munierujp@users.noreply.github.com>
2023-02-19 19:37:50 +0900

    Add icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,30 @@ export default defineNuxtConfig({
           name: 'description',
           content: 'Web app for calculating image size from aspect ratio'
         }
+      ],
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: '/favicon.ico'
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          href: '/favicon-256x256.png',
+          sizes: '256x256'
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          href: '/android-chrome-192x192.png',
+          sizes: '192x192'
+        },
+        {
+          rel: 'apple-touch-icon',
+          href: '/apple-touch-icon-180x180.png',
+          sizes: '180x180'
+        },
       ]
     }
   },
