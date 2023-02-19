fbe5a75b791132631540a53d861cb27fc70af322
Munieru <20086673+munierujp@users.noreply.github.com>
2023-02-19 19:44:52 +0900

    Add title

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -2,11 +2,13 @@
   <v-app-bar class="app-header bg-grey-darken-4 text-grey-lighten-5">
     <v-app-bar-title text="imgcalc" />
     <v-btn
+      title="Twitter"
       icon="mdi-twitter"
       href="https://twitter.com/munieru_jp"
       target="_blank"
     />
     <v-btn
+      title="GitHub"
       icon="mdi-github"
       href="https://github.com/munierujp/imgcalc"
       target="_blank"
