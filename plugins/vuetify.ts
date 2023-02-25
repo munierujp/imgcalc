58965a1e498ee24c3e20b8684d5a972e575b259d
Munieru <20086673+munierujp@users.noreply.github.com>
2023-02-25 21:49:52 +0900

    Update vuetify.ts

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,26 +1,8 @@
 import '@mdi/font/css/materialdesignicons.css'
 import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-import {
-  aliases,
-  mdi
-} from 'vuetify/iconsets/mdi'
+import { config } from '../config/vuetify'
 
 export default defineNuxtPlugin((nuxtApp) => {
-  const vuetify = createVuetify({
-    components,
-    directives,
-    icons: {
-      aliases,
-      defaultSet: 'mdi',
-      sets: {
-        mdi
-      }
-    },
-    theme: {
-      defaultTheme: 'dark'
-    }
-  })
+  const vuetify = createVuetify(config)
   nuxtApp.vueApp.use(vuetify)
 })
