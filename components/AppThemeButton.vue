f423f9122c8b02642541f6f5630261d0ce110e91
Munieru <20086673+munierujp@users.noreply.github.com>
2023-02-26 22:04:10 +0900

    Beautify

--- components/AppThemeButton.vue
+++ components/AppThemeButton.vue
@@ -3,6 +3,7 @@ import { useTheme } from 'vuetify'
 
 const theme = useTheme()
 const icon = computed(() => theme.global.current.value.dark ? 'mdi-weather-night' : 'mdi-weather-sunny')
+
 const handleClick = () => {
   theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
 }
