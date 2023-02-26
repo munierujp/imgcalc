7878e4229cd98f6a062937c2b615a7e7b26bc7c3
Munieru <20086673+munierujp@users.noreply.github.com>
2023-02-26 17:31:52 +0900

    Use AppGitHubButton

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -3,11 +3,6 @@
     <v-app-bar-title text="imgcalc" />
     <AppThemeButton />
     <AppTwitterButton />
-    <v-btn
-      title="GitHub"
-      icon="mdi-github"
-      href="https://github.com/munierujp/imgcalc"
-      target="_blank"
-    />
+    <AppGitHubButton />
   </v-app-bar>
 </template>
