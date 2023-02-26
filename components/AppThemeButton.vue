5e2c1de76d955b63c4ca9d437bb72511f475acf9
Munieru <20086673+munierujp@users.noreply.github.com>
2023-02-26 17:25:10 +0900

    Create AppThemeButton.vue

---
+++ components/AppThemeButton.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import { useTheme } from 'vuetify'
+
+const theme = useTheme()
+const icon = computed(() => theme.global.current.value.dark ? 'mdi-weather-night' : 'mdi-weather-sunny')
+const handleClick = () => {
+  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+}
+</script>
+
+<template>
+  <v-btn
+    title="toggle theme"
+    :icon="icon"
+    @click="handleClick"
+  />
+</template>
