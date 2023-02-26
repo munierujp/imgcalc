44339d573a2420349491ccb6ff7779dae7534c24
Munieru <20086673+munierujp@users.noreply.github.com>
2023-02-26 17:25:21 +0900

    Use AppThemeButton

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,21 +1,7 @@
-<script lang="ts" setup>
-import { useTheme } from 'vuetify'
-
-const theme = useTheme()
-const themeIcon = computed(() => theme.global.current.value.dark ? 'mdi-weather-night' : 'mdi-weather-sunny')
-const handleClickThemeButton = () => {
-  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
-}
-</script>
-
 <template>
   <v-app-bar>
     <v-app-bar-title text="imgcalc" />
-    <v-btn
-      title="toggle theme"
-      :icon="themeIcon"
-      @click="handleClickThemeButton"
-    />
+    <AppThemeButton />
     <v-btn
       title="Twitter"
       icon="mdi-twitter"
