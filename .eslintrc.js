021f5ad41acb331e3ab632ef177dd3f15ac7e249
Munieru <20086673+munierujp@users.noreply.github.com>
2023-03-28 23:24:51 +0900

    Add type to ESLint config (#302)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
-module.exports = {
+// @ts-check
+
+/** @typedef {import('eslint').ESLint.ConfigData} ConfigData */
+
+/**
+ * @type {ConfigData}
+ * @see https://eslint.org/docs/latest/use/configure/configuration-files
+ */
+const config = {
   root: true,
   extends: [
     '@munierujp/eslint-config-typescript',
@@ -8,3 +16,5 @@ module.exports = {
     project: './tsconfig.json'
   }
 }
+
+module.exports = config
